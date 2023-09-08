73e0dc63ad391db9066d3bac66691f60adee827a
aaron-pollet <pollet_aaron@hotmail.com>
2023-09-08 22:18:56 +0200

    initial app setup / cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # netflix-clone
+
+This is my first time trying to clone a web application on my own.
---
+++ netflix-clone/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Dit wordt de netflix clone!</h1>
+    </div>
+  );
+}
+
+export default App;
